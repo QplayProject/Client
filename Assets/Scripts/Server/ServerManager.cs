02b25f683fb00728423f9b1ce200098ae48682b9
pm.jaycci <pm.jaycci@gmail.com>
2024-02-12 02:54:57 +0900

    aws 연결 테스트중

--- Assets/Scripts/Server/ServerManager.cs
+++ Assets/Scripts/Server/ServerManager.cs
@@ -26,7 +26,7 @@ public class ServerManager : MonoBehaviour
     public TcpClient ChatTcpClient { get; set; }
     public NetworkStream ChatNetworkStream { get; set; }
     public string ChatServerIp = "13.125.254.231";//"127.0.0.1";
-    public int ChatServerPort = 12345;
+    public int ChatServerPort = 8080;
 
     public async Task SendMessageAsync(string message)
     {
